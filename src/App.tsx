bff98e548612192b8ed381f0d1cd428248591cee
Ninh Thai Lam <thailamstudy@gmail.com>
2024-08-07 11:21:21 +0700

    Show two separate lists

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { getImageUrl } from "./utils";
 
 export default function List () {
   const chemists = people.filter(person => person.profession === 'chemist')
-  const listItems = chemists.map(person =>
-    <li>
+  const otherPeople = people.filter(person => person.profession !== 'chemist')
+  const listChemist = chemists.map(person =>
+    <li key={person.id}>
       <img src={getImageUrl(person)} alt={person.name} />
       <p>
       <b>{person.name}:</b>
@@ -12,6 +13,24 @@ export default function List () {
         known for: {person.accomplishment}
       </p>
     </li>
+  )
+  const listOthers = otherPeople.map(person =>
+    <li key={person.id}>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+      <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for: {person.accomplishment}
+      </p>
+    </li>
+  )
+  return (
+    <>
+      <h1>Chemists</h1>
+      <ul>{listChemist}</ul>
+      <br/>
+      <h1>Everyone Else</h1>
+      <ul>{listOthers}</ul>
+    </>
   );
-  return <ul>{listItems}</ul>;
 }
